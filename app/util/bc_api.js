f8e1f7ab9415ea0739fe917623f24a66fdc00dd0
Aylan Mello <aylanismello@gmail.com>
2017-03-28 01:15:07 -0600

    cool update to point at new prod api url

--- app/util/bc_api.js
+++ app/util/bc_api.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 const localUrl = 'http://localhost:3000/api/v1/tracks/filter';
-const devUrl = 'https://bc-services.herokuapp.com/api/v1/tracks/filter';
+const devUrl = 'https://bc-fire-api.herokuapp.com/api/v1/tracks/filter';
 
 // let url = ((location.hostname === 'localhost') ? localUrl : devUrl);
 
