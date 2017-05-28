0a746308a65c1f2b52bc55ae6431eb37f33b4c9f
Aylan Mello <aylanismello@gmail.com>
2017-05-28 16:11:43 -0700

    looking responsive!

---
+++ app/components/navs/side_nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SideNav = (props) => {
+  return (
+    <div className="side-nav-container">
+      <div className="side-nav">
+
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
