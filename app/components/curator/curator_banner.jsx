029dd7c43836ff55a7ff4b1f23f05e6f948c07b8
Aylan Mello <aylanismello@gmail.com>
2017-08-05 02:12:15 -0600

    fixed more css bugs

--- app/components/curator/curator_banner.jsx
+++ app/components/curator/curator_banner.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import TagList from '../shared/tag_list';
 import Loading from '../loading';
 import { renderUserList } from '../../util/component_helpers';
 
 const CuratorBanner = ({ user }) => {
-	if(!user.top_publishers) {
+	if (!user.top_publishers) {
 		return (
 			<div>
 				<Loading />
 			</div>
-		)
+		);
 	} else {
 		return (
-			<div
-				className="thumbnail track-banner"
-			>
+			<div className="thumbnail track-banner">
 				<div className="left-side">
 					<h2> {user.name}</h2>
 					<img src={user.avatar_url} />
 					<h3> Most published artists </h3>
 					{renderUserList(user.top_publishers, 'publishers')}
 				</div>
+
+				<div className="right-side">
+					<TagList tagList={user.top_tags.slice(0, 5)} />
+				</div>
 			</div>
-		)
+		);
 	}
 };
 
--- app/components/publisher/publisher_banner.jsx
+++ app/components/publisher/publisher_banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TagList from '../shared/tag_list';
 import { renderUserList, renderUserHandles } from '../../util/component_helpers';
 
 const PublisherBanner = ({ user }) => {
@@ -12,6 +13,10 @@ const PublisherBanner = ({ user }) => {
 				{renderUserHandles(user.handles)}
 				<h3> Most selected by </h3>
 				{renderUserList(user.top_curators, 'curators')}
+			</div>
+
+			<div className="right-side">
+				<TagList tagList={user.top_tags.slice(0, 5)} />
 			</div>
 		</div>
 	);
