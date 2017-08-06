f8080c0eebc4e096505f8ab852ff0bc2ac10a7dc
Aylan Mello <aylanismello@gmail.com>
2017-08-06 00:45:08 -0600

    finally got loading to work on album art

--- app/components/shared/bc_album_art.jsx
+++ app/components/shared/bc_album_art.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import * as FontAwesome from 'react-icons/lib/fa/';
+import BCSpinner from './bc_spinner';
+
+const playIconUrl =
+	'https://cdn3.iconfinder.com/data/icons/seo-marketing-2-1/48/56-128.png';
+const pauseIconUrl =
+	'https://cdn1.iconfinder.com/data/icons/media-volume-1/48/017-512.png';
 
 const BCAlbumArt = ({
 	hasRanking,
@@ -14,23 +21,45 @@ const BCAlbumArt = ({
 		? track.artwork_url
 		: track.publisher.avatar_url;
 
-	const playIconUrl =
-		'https://cdn3.iconfinder.com/data/icons/seo-marketing-2-1/48/56-128.png';
-	const pauseIconUrl =
-		'https://cdn1.iconfinder.com/data/icons/media-volume-1/48/017-512.png';
-	const loadingIconUrl =
-		'https://cdn1.iconfinder.com/data/icons/loading-wait-time/256/loading_wait_time_02-128.png';
-	let playIcon = playIconUrl;
+	let showLoading = false;
+
+	let currentIcon = (
+		<img
+			onClick={handleTrackClick}
+			src={playIconUrl}
+			className="artwork-play cursor-pointer-grow"
+		/>
+	);
 
 	if (playingTrackId === trackId) {
 		if (trackLoaded && playing) {
-			playIcon = pauseIconUrl;
+			showLoading = false;
+			currentIcon = (
+				<img
+					onClick={handleTrackClick}
+					src={pauseIconUrl}
+					className="artwork-play cursor-pointer-grow"
+				/>
+			);
 		} else if (trackLoaded && !playing) {
-			playIcon = playIconUrl;
+			showLoading = false;
+			currentIcon = (
+				<img
+					onClick={handleTrackClick}
+					src={playIconUrl}
+					className="artwork-play cursor-pointer-grow"
+				/>
+			);
 		} else {
-			playIcon = loadingIconUrl;
+			// currentIcon = <BCSpinner extraClass="artwork-play" />;
+			currentIcon = null;
+			showLoading = true;
 		}
 	}
+
+	// const extraClass = showLoading ? 'artwork-play' : 'artwork-play display-none';
+	const extraClass = showLoading ? 'artwork-play' : 'artwork-play display-none';
+	// const extraClass = 'artwork-play';
 
 	return (
 		<div className="artwork-wrapper">
@@ -43,7 +72,9 @@ const BCAlbumArt = ({
 				: null}
 
 			<img src={artworkUrl} className="artwork-icon" />
-			<img onClick={handleTrackClick} src={playIcon} className="artwork-play" />
+			{currentIcon}
+			<BCSpinner extraClass={extraClass} />
+			{/* <img onClick={handleTrackClick} src={playIconUrl} className="artwork-play" /> */}
 		</div>
 	);
 };
--- app/components/shared/bc_spinner.jsx
+++ app/components/shared/bc_spinner.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Spinner from 'react-spinkit';
+import PropTypes from 'prop-types';
 
-const BCSpinner = () => (
-	<div className="react-loading-spinner-container">
-		<Spinner name="line-scale-pulse-out" className="react-loading-spinner" />
-	</div>
-);
+const BCSpinner = ({ extraClass }) => {
+	const className = `react-loading-spinner-container ${extraClass}`;
+
+	return (
+		<div className={className}>
+			<Spinner name="line-scale-pulse-out" className="react-loading-spinner" />
+			{/* <Spinner name="line-scale-pulse-out" className={className} /> */}
+		</div>
+	);
+};
+
+BCSpinner.defaultProps = {
+	extraClass: ''
+};
+
+BCSpinner.propTypes = {
+	extraClass: PropTypes.string
+};
 
 export default BCSpinner;
