b12a3a50dd02fa757b14f3d04d40692f891104ed
Aylan Mello <aylanismello@gmail.com>
2017-08-07 00:03:36 -0600

    done making pretty

--- app/components/shared/info_dropdown.jsx
+++ app/components/shared/info_dropdown.jsx
@@ -2,33 +2,71 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UserList from './user_list';
 
-const InfoDropdown = ({ children, users, userType, length }) => {
-	return (
-		<div className="info-dropdown-container">
-			<div className="info-dropdown">
-				<div className="actual-hover">
-					{children}
-				</div>
-				<div className="info-dropdown-content">
-					<div className="arrow-up" />
-					<UserList users={users} userType={userType} length={length} />
+const pagination = 8;
+
+class InfoDropdown extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.renderMoreContentButton = this.renderMoreContentButton.bind(this);
+		this.state = {
+			currentLength: pagination
+		};
+	}
+
+	renderMoreContentButton(length) {
+		return (
+			<h3
+				className="more-content-button"
+				onClick={() => {
+					if (this.state.currentLength < length) {
+						this.setState({
+							currentLength: this.state.currentLength + pagination
+						});
+					}
+				}}
+			>
+				{' '}+
+			</h3>
+		);
+	}
+
+	render() {
+		const { users, userType, children, length } = this.props;
+
+		return (
+			<div className="info-dropdown-container">
+				<div className="info-dropdown">
+					<div className="actual-hover">
+						{children}
+					</div>
+					<div className="info-dropdown-content">
+						<div className="arrow-up" />
+						<UserList
+							users={users}
+							userType={userType}
+							length={this.state.currentLength}
+						/>
+						{(this.state.currentLength < length)
+							? this.renderMoreContentButton(length)
+							: null}
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 InfoDropdown.defaultProps = {
 	users: [],
-	userType: 'curators',
-	length: 5
+	userType: 'curators'
 };
 
 InfoDropdown.propTypes = {
 	children: PropTypes.string.isRequired,
 	users: PropTypes.array,
-	userType: PropTypes.string,
-	length: PropTypes.number
+	userType: PropTypes.string.isRequired,
+	length: PropTypes.number.isRequired
 };
 
 export default InfoDropdown;
