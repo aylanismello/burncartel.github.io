0747f40843277286f6aa9c40ce16cf7c679d07fb
Aylan Mello <amello@malwarebytes.com>
2017-09-08 17:12:37 -0700

    lookin gooddd

--- app/middleware/user_middleware.js
+++ app/middleware/user_middleware.js
@@ -1,102 +1,121 @@
+/* global FB */
 import {
-  userConstants,
-  receiveCurrentUser,
-  setFBDidInit,
-  likeTrack,
-  unlikeTrack,
-  updateLikedTracks,
-  startLikePost,
-  endLikePost
+	userConstants,
+	receiveCurrentUser,
+	setFBDidInit,
+	likeTrack,
+	unlikeTrack,
+	updateLikedTracks,
+	startLikePost,
+	endLikePost
 } from '../actions/user_actions';
-import {
-  updateTrackLikeCount
-} from '../actions/feed_actions';
+import { updateTrackLikeCount } from '../actions/feed_actions';
 import { getFBUser } from '../util/login_api';
 import {
-  getUserTracksHash,
-  getFeedTracksHash,
-  getPlayingFeedTracksHash
+	getUserTracksHash,
+	getFeedTracksHash,
+	getPlayingFeedTracksHash
 } from '../selectors/track_selector';
 import { postLike } from '../util/like_api';
 
 const getNewLikeCount = (trackId, getState, incrementOrDecrement) => {
-	const track = getPlayingFeedTracksHash(getState())[trackId] || getFeedTracksHash(getState())[trackId];
-  if(incrementOrDecrement === 'inc') {
-    return track.num_likes + 1;
-  } else {
-    return track.num_likes - 1;
-  }
+	const track =
+		getPlayingFeedTracksHash(getState())[trackId] || getFeedTracksHash(getState())[trackId];
+	if (incrementOrDecrement === 'inc') {
+		return track.num_likes + 1;
+	} else {
+		return track.num_likes - 1;
+	}
 };
 
-const UserMiddleware = ({ getState, dispatch }) => next => action => {
-  switch(action.type) {
-    case userConstants.LIKE_UNLIKE_TRACK:
-      const userTracks = getUserTracksHash(getState());
+const UserMiddleware = ({ getState, dispatch }) => next => (action) => {
+	switch (action.type) {
+		case userConstants.LIKE_UNLIKE_TRACK:
+			const userTracks = getUserTracksHash(getState());
 
-      if(userTracks[action.trackId] === undefined) {
-        dispatch(likeTrack(action.trackId));
-      } else {
-        dispatch(unlikeTrack(action.trackId));
-      }
-      return next(action);
-    case userConstants.LIKE_TRACK:
-      dispatch(startLikePost());
+			if (userTracks[action.trackId] === undefined) {
+				dispatch(likeTrack(action.trackId));
+			} else {
+				dispatch(unlikeTrack(action.trackId));
+			}
+			return next(action);
+		case userConstants.LIKE_TRACK:
+			dispatch(startLikePost());
 
-      postLike({ create: true }, action.trackId, getState().user.currentUser.id, (createdLike) => {
-        dispatch(updateLikedTracks(createdLike.tracks));
+			postLike(
+				{ create: true },
+				action.trackId,
+				getState().user.currentUser.id,
+				(createdLike) => {
+					dispatch(updateLikedTracks(createdLike.tracks));
 
-        // const oneMoreLike = getClickedTrack(action.trackId, getState).num_likes + 1;
-        // const oneMoreLike = getPlayingFeedTracksHash(getState())[action.trackId].num_likes + 1;
+					// const oneMoreLike = getClickedTrack(action.trackId, getState).num_likes + 1;
+					// const oneMoreLike = getPlayingFeedTracksHash(getState())[action.trackId].num_likes + 1;
 
-        dispatch(updateTrackLikeCount(action.trackId, getNewLikeCount(action.trackId, getState, 'inc')));
+					dispatch(
+						updateTrackLikeCount(action.trackId, getNewLikeCount(action.trackId, getState, 'inc'))
+					);
 
-        dispatch(endLikePost());
-      }, (err) => {
-        throw new Error(`omg hit this error creating like ${err}`);
-      });
+					dispatch(endLikePost());
+				},
+				(err) => {
+					throw new Error(`omg hit this error creating like ${err}`);
+				}
+			);
 
-      return next(action);
-    case userConstants.UNLIKE_TRACK:
-      dispatch(startLikePost());
+			return next(action);
+		case userConstants.UNLIKE_TRACK:
+			dispatch(startLikePost());
 
-      postLike({ create: false }, action.trackId, getState().user.currentUser.id, (createdLike) => {
-        dispatch(updateLikedTracks(createdLike.tracks));
+			postLike(
+				{ create: false },
+				action.trackId,
+				getState().user.currentUser.id,
+				(createdLike) => {
+					dispatch(updateLikedTracks(createdLike.tracks));
 
-        // const oneLessLike = getClickedTrack(action.trackId, getState).num_likes - 1;
-        dispatch(updateTrackLikeCount(action.trackId, getNewLikeCount(action.trackId, getState, 'dec')));
+					// const oneLessLike = getClickedTrack(action.trackId, getState).num_likes - 1;
+					dispatch(
+						updateTrackLikeCount(action.trackId, getNewLikeCount(action.trackId, getState, 'dec'))
+					);
 
-        dispatch(endLikePost());
-      }, (err) => {
-        throw new Error(`omg hit this error destroying like ${err}`);
-      });
+					dispatch(endLikePost());
+				},
+				(err) => {
+					throw new Error(`omg hit this error destroying like ${err}`);
+				}
+			);
 
-      return next(action);
-    case userConstants.INIT_FB:
-      window.fbAsyncInit = () => {
-        FB.init({
-          appId: '156389341554296',
-          cookie: true
-        });
+			return next(action);
+		case userConstants.INIT_FB:
+			if (getState().user.currentUser.id) {
+				dispatch(setFBDidInit());
+			} else {
+				window.fbAsyncInit = () => {
+					FB.init({
+						appId: '156389341554296',
+						cookie: true
+					});
 
-        FB.getLoginStatus((response) => {
-          if (response.status === 'connected') {
-            getFBUser(user => dispatch(receiveCurrentUser(user)));
-          }
-          dispatch(setFBDidInit());
-        });
-
-      };
-      return next(action);
-    case userConstants.LOGIN_FB:
-      FB.login(response => {
-        if(response.authResponse.accessToken) {
-          getFBUser(user => dispatch(receiveCurrentUser(user)));
-        }
-      });
-      return next(action);
-    default:
-      return next(action);
-  }
+					FB.getLoginStatus((response) => {
+						if (response.status === 'connected') {
+							getFBUser(user => dispatch(receiveCurrentUser(user)));
+						}
+						dispatch(setFBDidInit());
+					});
+				};
+			}
+			return next(action);
+		case userConstants.LOGIN_FB:
+			FB.login((response) => {
+				if (response.authResponse.accessToken) {
+					getFBUser(user => dispatch(receiveCurrentUser(user)));
+				}
+			});
+			return next(action);
+		default:
+			return next(action);
+	}
 };
 
 export default UserMiddleware;
